99bdc7c06bb1dc75e83fb9cd02966c0179af3172
Deepnoa <fukazawa.shu@deepnoa.com>
2026-03-04 18:45:19 +0900

    fix: always register nas tools in plugin

--- extensions/nas/index.ts
+++ extensions/nas/index.ts
@@ -6,13 +6,5 @@ import type {
 import { createNasTools } from "./src/nas-tools.js";
 
 export default function register(api: OpenClawPluginApi) {
-  api.registerTool(
-    ((ctx) => {
-      if (ctx.sandboxed) {
-        return null;
-      }
-      return createNasTools(api) as AnyAgentTool[];
-    }) as OpenClawPluginToolFactory,
-    { optional: true },
-  );
+  api.registerTool((() => createNasTools(api) as AnyAgentTool[]) as OpenClawPluginToolFactory);
 }
